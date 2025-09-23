845cd0bb30f7d445e19be24a0ae976a499fa5c4c
thatskali <140678159+thatskali@users.noreply.github.com>
2025-09-22 18:40:11 -0600

    Delete Proyecto 1/tang9k-hamming directory

--- Proyecto 1/tang9k-hamming/src/module_decoder.sv
+++
@@ -1,9 +0,0 @@
-module decoder (
-    input  logic ag, bg, cg, dg,    // Gray
-    output logic ab, bb, cb, db     // Binario
-);
-    assign ab = ag;
-    assign bb = ag ^ bg;
-    assign cb = (ag ^ bg) ^ cg;
-    assign db = ((ag ^ bg) ^ cg) ^ dg;
-endmodule
